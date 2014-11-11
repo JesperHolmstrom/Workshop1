4430e8b0e4c323f2e96b1d121829285b00925496
JesperHolmstrom <jesper_jeppe@hotmail.com>
2014-11-11 14:01:26 +0100

    GREEN
    Signed-off-by: JesperHolmstrom <jesper_jeppe@hotmail.com>

--- src/src/Item.java
+++ src/src/Item.java
@@ -1,8 +1,20 @@
 package src;
 
 public class Item {
+	private String product;
+	private int price;
+	
 	public Item(String product, int price) {
-		
+		this.product = product;
+		this.price = price;
+	}
+	
+	public String getProduct(){
+		return product;
+	}
+	
+	public int getPrice(){
+		return price;
 	}
 
 }
