d0505714703e813586f00260a380aa8dddef9848
JesperHolmstrom <jesper_jeppe@hotmail.com>
2014-11-11 14:28:32 +0100

    GREEN
    Signed-off-by: JesperHolmstrom <jesper_jeppe@hotmail.com>

--- src/src/Register.java
+++ src/src/Register.java
@@ -1,7 +1,11 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Register {
 	private int sum;
+	List<Item> items = new ArrayList<Item>();
 	
 	public Register() {
 		sum = 0;
@@ -13,7 +17,11 @@ public class Register {
 	
 	public void addItem(Item i){
 		sum += i.getPrice();
+		items.add(i);
 	}
 	
+	public List<Item> getItems(){
+		return items;
+	}
 
 }
