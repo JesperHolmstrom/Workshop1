e4974b8482b92fa019ac628aadea5c7704ffdc09
JesperHolmstrom <jesper_jeppe@hotmail.com>
2014-11-11 14:15:25 +0100

    GREEN
    Signed-off-by: Eleonor Lagercrantz

---
+++ src/src/Register.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Register {
+	private int sum;
+	
+	public Register() {
+		sum = 0;
+	}
+	
+	public int getSum() {
+		return sum;
+	}
+	
+
+}
--- src/tests/RegisterTest.java
+++ src/tests/RegisterTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import src.Register;
+
 public class RegisterTest {
 
 	@Test
