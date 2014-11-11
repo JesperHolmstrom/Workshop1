b392ab52f666614876000ecb02edc9738b8f23b8
JesperHolmstrom <jesper_jeppe@hotmail.com>
2014-11-11 13:51:41 +0100

    GREEN
    Signed-off-by: Eleonor Lagercrantz

---
+++ src/src/Item.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Item {
+	public Item(String product, int price) {
+		
+	}
+
+}
--- src/tests/ItemTest.java
+++ src/tests/ItemTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import src.Item;
+
 public class ItemTest {
 
 	@Test
