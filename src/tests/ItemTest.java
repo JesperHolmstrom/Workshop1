788c7ccf12a78c1d5addd629de6139f56d5fe280
JesperHolmstrom <jesper_jeppe@hotmail.com>
2014-11-11 13:55:35 +0100

    REFACTOR
    Signed-off-by: JesperHolmstrom <jesper_jeppe@hotmail.com>

--- src/tests/ItemTest.java
+++ src/tests/ItemTest.java
@@ -1,15 +1,13 @@
 package tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 import src.Item;
 
 public class ItemTest {
 
 	@Test
-	public void test() {
+	public void shouldCreateInstance() {
 		Item i = new Item("Milk", 30);
 	}
 
