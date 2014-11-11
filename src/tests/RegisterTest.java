05334941256443d7077e4020f68e08768709492e
JesperHolmstrom <jesper_jeppe@hotmail.com>
2014-11-11 14:24:57 +0100

    RED
    Signed-off-by: Eleonor Lagercrantz

--- src/tests/RegisterTest.java
+++ src/tests/RegisterTest.java
@@ -2,6 +2,9 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import src.Item;
@@ -21,6 +24,16 @@ public class RegisterTest {
 		Item milk = new Item("Milk", 30);
 		register.addItem(milk);
 		assertEquals(30, register.getSum());
+	}
+	
+	@Test
+	public void shouldAddItemToRegister() {
+		Register register = new Register();
+		List<Item> items = new ArrayList<Item>();
+		Item milk = new Item("Milk", 30);
+		items.add(milk);
+		register.addItem(milk);
+		assertEquals(items, register.getItems());
 	}
 
 }
