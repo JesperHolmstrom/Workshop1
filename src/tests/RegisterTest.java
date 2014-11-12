158be68cfe5aa0ffb3209ffe937b44794fb05b86
JesperHolmstrom <jesper_jeppe@hotmail.com>
2014-11-12 16:23:17 +0100

    RED
    Signed-off-by: JesperHolmstrom <jesper_jeppe@hotmail.com>

--- src/tests/RegisterTest.java
+++ src/tests/RegisterTest.java
@@ -2,10 +2,12 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import src.Item;
 import src.Register;
@@ -48,5 +50,19 @@ public class RegisterTest {
 		assertEquals("Milk 30, Eggs 25, Candy 10, Nr of items 3, Sum 65", register.getReceipt());
 	}
 	
+	@Test
+	public void shouldPrintReceipt(){
+		Register register = new Register();
+		Item milk = new Item("Milk", 30);
+		Item eggs = new Item("Eggs", 25);
+		Item candy = new Item("Candy", 10);
+		register.addItem(milk);
+		register.addItem(eggs);
+		register.addItem(candy);
+		String receipt = register.getReceipt();
+		PrintStream printStream = Mockito.mock(PrintStream.class);
+		register.printReceipt(receipt, printStream);
+		Mockito.verify(printStream).println(receipt);
+	}
 
 }
